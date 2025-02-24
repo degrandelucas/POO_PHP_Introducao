a29c5dc4da96cf4226c1142c81d43e07803e399b
degrandelucas <lucasdegrande15@gmail.com>
2025-02-24 19:26:40 -0300

    first commit of project, creat class Conta

---
+++ Conta.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Conta;
+
+class Conta
+{
+    private int $saldoEmCentavos;
+    private string $nomeTitular;
+    private string $numeroConta;
+
+    public function setSaldoEmCentavos(int $saldoEmCentavos): void
+    {
+        $this->saldoEmCentavos = $saldoEmCentavos;
+    }
+
+    public function getSaldoEmCentavos(): int
+    {
+        return $this->saldoEmCentavos;
+    }
+
+    public function setNomeTitular(string $nomeTitular): void
+    {
+        $this->nomeTitular = $nomeTitular;
+    }
+
+    public function getNomeTitular(): string
+    {
+        return $this->nomeTitular;
+    }
+
+    public function setNumeroConta(int $numeroConta): void
+    {
+        $this->numeroConta = $numeroConta;
+    }
+
+    public function getNumeroConta(): string
+    {
+        return $this->numeroConta;
+    }
+}
---
+++ MovimentacaoBancaria.php
@@ -0,0 +1,5 @@
+<?php
+
+require 'Conta.php';
+
+
